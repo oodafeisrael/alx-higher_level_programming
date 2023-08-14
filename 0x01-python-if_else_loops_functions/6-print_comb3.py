408362dc4e70ca2483e8106b64d2267a2c347e48
oodafeisrael <oodafeisrael@yahoo.com>
2023-08-14 09:39:49 -0700

    task 6 commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for tens_dgt in range(9):
     for unit_dgt in range(tens_dgt + 1, 10):
-          print("{:d}{:d}".format(tens_dgt, unit_dgt), end=", ")
+        if tens_dgt * 10 + unit_dgt < 89:
+            print("{:d}{:d}".format(tens_dgt, unit_dgt), end=", ")
 print("{:d}".format(89))
