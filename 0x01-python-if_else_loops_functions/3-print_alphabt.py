4fb8be41e1ca43ae509fa6a46b297a2928c61e8c
oodafeisrael <oodafeisrael@yahoo.com>
2023-08-14 08:12:31 -0700

    task 3 commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for alph in range(ord('a'), ord('z')):
+for alph in range(ord('a'), ord('z') + 1):
     if (alph == ord('e') or (alph == ord('q'))):
         continue
     print(chr(alph).format(), end="")
