3b22214270e93355c1001f208b1a2a55cd624f6b
oodafeisrael <oodafeisrael@yahoo.com>
2023-08-15 08:34:11 -0700

    task 11 commit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,6 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-
-    new_list = []
-    for x in range(len(my_list)):
-        if x != idx:
-            new_list.append(my_list[x])
-    return new_list
+    else:
+        del(my_list[idx])
+    return my_list
