37e19d4bd8094dcc83518f248530a9cf6ad06beb
oodafeisrael <oodafeisrael@yahoo.com>
2023-09-15 09:05:48 -0700

    task 6 commit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,6 +3,9 @@ import json
 
 def load_from_json_file(filename):
     """Load an object from a JSON file and return it."""
-    with open(filename, 'r') as file:
-        obj = json.load(file)
-    return obj
+    try:
+        with open(filename, 'r') as file:
+            obj = json.load(file)
+        return obj
+    except json.JSONDecodeError as e:
+        raise ValueError(f'{e.msg}: line {e.lineno} column {e.colno} (char {e.pos})')
