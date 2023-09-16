31a86449f0273588d3d1f91821055e8329c8c871
oodafeisrael <oodafeisrael@yahoo.com>
2023-09-16 10:22:23 -0700

    task 1 commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,12 +5,12 @@
 class Base:
     """Base class for managing id attribute."""
 
-    __nb_objects = 0  # Private class attribute to manage object counts
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """Initialize a Base instance with a unique id or auto-incremented id."""
         if id is not None:
-            self.id = id  # Assign id if provided
+            self.id = id
         else:
-            Base.__nb_objects += 1  # Increment the object count
-            self.id = Base.__nb_objects  # Assign the new id
+            Base.__nb_objects += 1  
+            self.id = Base.__nb_objects
