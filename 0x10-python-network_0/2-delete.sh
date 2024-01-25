5a61adb0f72cc384f3fe3e79cd59868272a6b485
oodafeisrael <oodafeisrael@yahoo.com>
2024-01-25 13:03:17 +0100

    tasks update

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that sends a DELETE request to the URL passed as the first
-# argument and displays the body of the response
+# A Bash script that sends DELETE request to the URL passed as the first argument and displays the body of the response
 curl -s "$1" -X DELETE
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL as an argument, sends a GET request to
-# the URL, and displays the body of the response.
+# A Bash script that takes in a URL as an argument, sends GET request to the URL, and displays the body of the response.
 curl -s "$1" -X GET -H "X-School-User-Id: 98"
